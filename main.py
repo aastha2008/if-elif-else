8cec1614451127cd98a352cef05672d15d4dc4ce
Bhagat Aastha <109658@eesd.org>
2021-01-29 00:32:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,92 @@
+'''
+01/28/2021
+
+Review:
+
+if/else conditional statement
+
+if (Condition): 
+  Body Statement1
+else:
+  Body Statement2 
+
+Condition:
+  - Boolean Expression: True/False
+
+  a=5
+  b=int(input())
+  a==b
+  1>=0
+  
+if/elif/else conditional statement
+- used for decision-making operations with conditions 
+
+Formula:
+if (Condition):
+  Body Statement
+
+elif (Condition):
+  Body Statement
+
+
+elif (Condition):
+  Body Statement
+
+
+.  
+
+.
+
+.
+
+else:
+    Body Statement
+
+
+Condition:
+An expression that evaluates to produce a result which is a Boolean value (Boolean expression)
+
+In a conditional statement,
+1x if header
+
+1x(optional) else clause
+ (Miniumn) none
+
+
+#Ex
+
+a=0
+
+if(a == 0):
+  print("hi")
+  if(a==0):
+   print("hello")
+
+elif (a != 2):
+  print("welcome")
+
+else:
+  print("bye")
+
+
+
+a=5000
+num=int(input("Enter a number: "))
+
+if (num > 0 ): 
+  print("I see that your number is positive")
+elif (num==0):
+  print("You entered 0")
+else:
+  print("It is negative")
+'''
+age=int(input("What is your age: "))
+if (age >= 17):
+  print("You can watch all movies")
+elif (age >= 13):
+  print("You can watch PG-13/PG/G movies")
+else:
+  print("You can only watch PG/G movies")
+
+
+
